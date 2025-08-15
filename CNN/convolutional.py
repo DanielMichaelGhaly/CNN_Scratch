09804e4289cff2c91f037aa21aad86543110afb2
Daniel M. Hennawi <151990013+DanielMichaelGhaly@users.noreply.github.com>
2025-08-15 13:16:00 +0300

    implemented the backward propagation of the cnn layer

--- CNN/convolutional.py
+++ CNN/convolutional.py
@@ -3,6 +3,7 @@ from layer import Layer
 from scipy import signal
 
 class Convolutional(Layer):
+    # depth is number of kernel layers we have and therefore depth of output
     def __init__(self, input_shape, kernel_size, depth):
         input_depth, input_height, input_width = input_shape
         self.depth = depth
@@ -22,4 +23,13 @@ class Convolutional(Layer):
         return self.output
 
     def backward(self, output_gradient, learning_rate):
-        pass
+        kernels_gradient = np.zeros(self.kernels_shape)
+        input_gradient = np.zeros(self.input_shape)
+
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                kernels_gradient[i, j] = signal.correlate2d(self.input[j], output_gradient[i], "valid")
+                input_gradient[j] += signal.convolve2d(output_gradient[i], self.kernels[i, j], "full")
+            self.kernels -= learning_rate * kernels_gradient
+            self.biases -= learning_rate * output_gradient
+            return input_gradient
